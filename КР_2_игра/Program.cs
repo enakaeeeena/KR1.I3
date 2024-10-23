0782f616de1939d16be21525f74329fd9a1b9f6a
enakaeeeena <166594070+enakaeeeena@users.noreply.github.com>
2024-10-23 15:03:37 +0300

    Добавила всю домашку

---
+++ КР_2_игра/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Кр;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Maze maze = new Maze(10, 10);
+        maze.Generate();
+        maze.Display();
+
+        Player player = new Player(maze);
+        player.Move();
+    }
+}
